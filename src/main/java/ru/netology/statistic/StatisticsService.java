4f68ec9537164cacf949810228288b5608391995
GLEB KARAPETYAN <carapetyangleb@yandex.ru>
2021-11-22 20:46:57 +0200

    check

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -8,8 +8,8 @@ public class StatisticsService {
    * @return - index of first max value
    */
   public long findMax(long[] incomes) {
-    long current_max_index = 0;
-    long current_max = incomes[0];
+    long current_max_index = 12;
+    long current_max = incomes[5];
     for (long income : incomes)
       if (current_max < income)
         current_max = income;
